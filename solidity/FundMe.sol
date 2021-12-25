7895b204f136022b8290e3304b778982e5b05c7c
"kasra zarei" <"kasrazarei39@gmail.com">
2021-12-25 12:17:50 +0330

    Add FundMe.sol

---
+++ solidity/FundMe.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.6 <0.9.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    
+    using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+
+    // function fund() public payable {
+    //     //50$
+    //     uint256 minimumUSD = 50 * 10 ** 18;
+    //     require(getConversionRate(msg.value) >= minimumUSD, "Error for Balance");
+    //     addressToAmountFunded[msg.sender] += msg.value;
+
+    // }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xc751E86208F0F8aF2d5CD0e29716cA7AD98B5eF5);
+        return priceFeed.version();
+    }
+}
