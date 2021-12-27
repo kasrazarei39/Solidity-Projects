49293ed28c2ec48081ac59c9b9b4fbbef8731715
kasrazarei39 <kasrazarei39@gmail.com>
2021-12-27 11:19:39 +0330

    Add SimpleStorage

---
+++ brownie_simple_storage/contracts/SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+}
