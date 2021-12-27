85ac93da7c5d6822b9640387b79a16c36694c774
kasrazarei39 <kasrazarei39@gmail.com>
2021-12-27 12:50:15 +0330

    Use .env for add accounts

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,14 +1,18 @@
 from brownie import accounts
+import os
 
 
 def deploy_simple_storate():
     # account = accounts[0]
 
     # Create Account in safe mode:
-    # brownie accounts new 'name'
+    # commad: brownie accounts new 'name'
     # After that insert your private key and password
     # You can see accounts with this command: brownie accounts list
-    account = accounts.load("kasra-test")
+    # account = accounts.load("kasra-test")
+
+    # Or we can use .env
+    account = accounts.add(os.getenv("PRIVATE_KEY"))
     print(account)
 
 
