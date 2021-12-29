393cc8cb83de4de887cf53f4d92dae5e485f59bb
kasrazarei39 <kasrazarei39@gmail.com>
2021-12-29 11:38:24 +0330

    Pass priceFeed for deploy Contract

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,11 +1,20 @@
-from brownie import FundMe
+from brownie import FundMe, config, network
 from scripts.helpful_scripts import get_account
+
 
 def deploy_fund_me():
     account = get_account()
+    # pass the price feed address to our fundme contract
+    # if we are on a persistent network like rinkeby, use the assocated address
+    # otherwise, deploy mocks
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active(
+        )]["eth_usd_price_feed"]
+    else:
+        pass
     fund_me = FundMe.deploy({"from": account}, publish_source=True)
     print(f"Contract deployed to {fund_me.address}")
-    
-    
+
+
 def main():
     deploy_fund_me()
