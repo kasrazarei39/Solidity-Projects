9ea425aeb19c95f78f3ff171209fa4858e2e3f37
kasrazarei39 <kasrazarei39@gmail.com>
2021-12-29 11:59:29 +0330

    Add development config for deploy

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import FundMe, config, network
+from brownie import FundMe, MockV3Aggregator, config, network
 from scripts.helpful_scripts import get_account
 
 
@@ -11,8 +11,13 @@ def deploy_fund_me():
         price_feed_address = config["networks"][network.show_active(
         )]["eth_usd_price_feed"]
     else:
-        pass
-    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+        mock_aggregaor = MockV3Aggregator.deploy(
+            18, 2000000000000000000, {"from": account})
+        price_feed_address = mock_aggregaor.address
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"))
     print(f"Contract deployed to {fund_me.address}")
 
 
