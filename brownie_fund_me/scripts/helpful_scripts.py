ee8e2e606d92bc890d205eb1c568a14b6ce026bd
kasrazarei39 <kasrazarei39@gmail.com>
2021-12-30 13:48:50 +0330

    Create Const variable

--- brownie_fund_me/scripts/helpful_scripts.py
+++ brownie_fund_me/scripts/helpful_scripts.py
@@ -1,6 +1,9 @@
 from brownie import network, accounts, config, MockV3Aggregator
 from web3 import Web3
 
+DECIMALS = 18
+STARTING_PRICE = 2000
+
 
 def get_account():
     if network.show_active() == "development":
@@ -12,6 +15,6 @@ def get_account():
 def deploy_mocks():
     # It can be deployed only once
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(18, Web3.toWei(
-            2000, "ether"), {"from": get_account()})
+        MockV3Aggregator.deploy(DECIMALS, Web3.toWei(
+            STARTING_PRICE, "ether"), {"from": get_account()})
     price_feed_address = MockV3Aggregator[-1].address  # Use last Deployed
