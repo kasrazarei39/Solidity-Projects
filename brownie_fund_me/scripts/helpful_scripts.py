efbb135b90c79005b01b914cf55573e085ec0e4b
kasrazarei39 <kasrazarei39@gmail.com>
2021-12-30 14:08:09 +0330

    Add local brownie networks

--- brownie_fund_me/scripts/helpful_scripts.py
+++ brownie_fund_me/scripts/helpful_scripts.py
@@ -1,12 +1,14 @@
 from brownie import network, accounts, config, MockV3Aggregator
 from web3 import Web3
 
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
 DECIMALS = 18
 STARTING_PRICE = 2000
 
 
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
@@ -17,4 +19,3 @@ def deploy_mocks():
     if len(MockV3Aggregator) <= 0:
         MockV3Aggregator.deploy(DECIMALS, Web3.toWei(
             STARTING_PRICE, "ether"), {"from": get_account()})
-    price_feed_address = MockV3Aggregator[-1].address  # Use last Deployed
